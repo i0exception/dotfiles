c7d317d78c750bae07a50d8cef56dc505475d6e0
Aniruddha <i0exception@yahoo.com>
2016-05-20 00:54:30 +0000

    Add gocode support

--- install_packages.sh
+++ install_packages.sh
@@ -1,8 +1,37 @@
-PACKAGES="g++ python dstat htop"
+PACKAGES="g++ python python-dev dstat htop cmake tmux"
+GOLANG="https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz"
+GOLANG_ARTIFACT="go1.6.2.linux-amd64.tar.gz"
 
-for p in $(echo $PACKAGES | tr ' ' '\n'); do
-    dpkg -s $p 2>&1 > /dev/null
-    if [ $? -ne 0 ]; then
-        sudo aptitude install $p
-    fi
-done
+install_packages() {
+    for p in $(echo $PACKAGES | tr ' ' '\n'); do
+        dpkg -s $p 2>&1 > /dev/null
+        if [ $? -ne 0 ]; then
+            sudo aptitude install $p
+        fi
+    done
+}
+
+install_golang() {
+    cd /tmp/
+    wget --quiet -O $GOLANG_ARTIFACT $GOLANG
+    sudo rm -f /usr/local/go
+    sudo tar -C /usr/local -xzf $GOLANG_ARTIFACT
+    rm $GOLANG_ARTIFACT
+
+    mkdir -p $HOME/.gotools/src
+    GOPATH=$HOME/.gotools /usr/local/go/bin/go get -u github.com/FiloSottile/gvt github.com/jteeuwen/go-bindata/... github.com/golang/lint/golint
+}
+
+install_vimgo() {
+    cd ~/.vim/bundle
+    [[ -d vim-go ]] || git clone https://github.com/fatih/vim-go.git
+}
+
+install_vim_ycm() {
+    cd ~/.vim/bundle
+    [[ -d YouCompleteMe ]] || git clone https://github.com/Valloric/YouCompleteMe.git
+    cd YouCompleteMe
+    git submodule update --init --recursive
+    ./install.py --gocode-completer
+}
+
