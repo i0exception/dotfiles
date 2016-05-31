ec466016f6a0ba4de64a9cf7578c35be2eacfa77
Aniruddha <i0exception@yahoo.com>
2016-05-31 22:46:41 +0000

    Add zsh

--- install_packages.sh
+++ install_packages.sh
@@ -1,4 +1,4 @@
-PACKAGES="g++ python python-dev dstat htop cmake tmux fabric python-pip"
+PACKAGES="g++ python python-dev dstat htop cmake tmux fabric python-pip zsh"
 GOLANG="https://storage.googleapis.com/golang/go1.6.2.linux-amd64.tar.gz"
 GOLANG_ARTIFACT="go1.6.2.linux-amd64.tar.gz"
 
